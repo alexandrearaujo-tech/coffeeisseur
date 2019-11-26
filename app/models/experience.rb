513801d1bc96e1a30858c8996baaae530cc27d12
Alexandre Araujo <alelaraujo@gmail.com>
2019-11-26 16:04:23 +0000

    change the place model

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -3,5 +3,4 @@ class Experience < ApplicationRecord
   belongs_to :place
   has_many :bookings
   has_many :user_experiences
-  has_many :reviews, through: :bookings
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,10 @@
 class Place < ApplicationRecord
   has_many :cards
   has_many :experiences
+  has_many :bookings, through: :experiences
   has_many :user_places
 
   def reviews
-    Review.joins(bookings: :experiences).where(place_id: self.id)
+    Review.joins(booking: :experience).where("experiences.place_id = ?", self.id)
   end
 end
