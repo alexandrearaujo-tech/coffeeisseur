522722203e2fb24cd634bcea434f35da4d39177f
Sharon Moriel <shmoriel@gmail.com>
2019-11-28 10:05:49 +0000

    profile page layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,70 +16,68 @@ datePickers.forEach(element => {
   });
 });
 
+
+// profile page
+
+$(function() {
+  $('.toggle-btn').click(function() {
+    $('.filter-btn').toggleClass('open');
+
+  });
+
+  $('.filter-btn a').click(function() {
+    $('.filter-btn').removeClass('open');
+
+  });
+
+});
+
+const x = document.getElementsByClassName("menus")
+
+
+document.getElementById("one").addEventListener ("click", displayFavs);
+document.getElementById("two").addEventListener ("click", displayBookings);
+document.getElementById("three").addEventListener ("click", displayCards);
+document.getElementById("all").addEventListener ("click", displayProfile);
+
+
+function displayFavs() {
+  var i;
+  var x = document.getElementsByClassName("menus");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById("favs").style.display = "block";
+}
+
+function displayBookings() {
+  var i;
+  var x = document.getElementsByClassName("menus");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById("bookings").style.display = "block";
+}
+
+function displayCards() {
+  var i;
+  var x = document.getElementsByClassName("menus");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById("cards").style.display = "block";
+}
+
+function displayProfile() {
+  var i;
+  var x = document.getElementsByClassName("menus");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById("profile").style.display = "block";
+}
+
 loadDynamicBannerText();
-
-
-
-// // profile page
-
-// $(function() {
-//   $('.toggle-btn').click(function() {
-//     $('.filter-btn').toggleClass('open');
-
-//   });
-
-//   $('.filter-btn a').click(function() {
-//     $('.filter-btn').removeClass('open');
-
-//   });
-
-// });
-
-// const x = document.getElementsByClassName("menus")
-
-// if(x != 'undefined'){
-//   // document.getElementById("one").addEventListener ("click", displayFavs);
-//   document.getElementById("two").addEventListener ("click", displayBookings);
-//   // document.getElementById("three").addEventListener ("click", displayCards);
-//   // document.getElementById("all").addEventListener ("click", displayProfile);
-// }
-
-// function displayFavs() {
-//   var i;
-//   var x = document.getElementsByClassName("menus");
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";
-//   }
-//   document.getElementById("favs").style.display = "block";
-// }
-
-// function displayBookings() {
-//   var i;
-//   var x = document.getElementsByClassName("menus");
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";
-//   }
-//   document.getElementById("bookings").style.display = "block";
-// }
-
-// function displayCards() {
-//   var i;
-//   var x = document.getElementsByClassName("menus");
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";
-//   }
-//   document.getElementById("cards").style.display = "block";
-// }
-
-// function displayProfile() {
-//   var i;
-//   var x = document.getElementsByClassName("menus");
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";
-//   }
-//   document.getElementById("profile").style.display = "block";
-// }
-
 
 
 //  const imgBtn = document.querySelector('.img__btn')
