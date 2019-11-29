cc305a4cdd5b9c487fcecc51164e6e1cfd2ab85a
kevin richardson <richalk@gmail.com>
2019-11-29 10:21:15 +0000

    Fixed Mapbox CSS

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -13,7 +13,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/outdoors-v11'
+      style: 'mapbox://styles/mapbox/streets-v10'
     });
 
     const markers = JSON.parse(mapElement.dataset.markers);
@@ -30,42 +30,7 @@ const initMapbox = () => {
   }
 };
 
-// const fitMapToMarkers = (map, markers) => {
-//   const bounds = new mapboxgl.LngLatBounds();
-//   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-//   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-// };
 
-// const addMarkersToMap = (map, markers) => {
-//   markers.forEach((marker) => {
-//     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
-
-//     new mapboxgl.Marker()
-//       .setLngLat([ marker.lng, marker.lat ])
-//       .setPopup(popup) // add this
-//       .addTo(map);
-//   });
-// };
-
-// const initMapbox = () => {
-//   const mapElement = document.getElementById('map');
-
-//   if (mapElement) {
-//     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-//     const map = new mapboxgl.Map({
-//       container: 'map',
-//       style: 'mapbox://styles/mapbox/outdoors-v11'
-//     });
-//     const markers = JSON.parse(mapElement.dataset.markers);
-//   markers.forEach((marker) => {
-//     new mapboxgl.Marker()
-//       .setLngLat([ marker.lng, marker.lat ])
-//       .setPopup(popup) // add this
-//       .addTo(map);
-//   });
-//   fitMapToMarkers(map, markers);
-//   }
-// };
 
 
 
