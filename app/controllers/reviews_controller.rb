698a92aecfdcdf03f9f5582bf9ae0c5647cb05b2
Alexandre Araujo <alelaraujo@gmail.com>
2019-12-02 19:58:35 +0000

    cards

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -16,24 +16,9 @@ class ReviewsController < ApplicationController
     authorize @review
     @experience = Experience.find(@booking.experience_id)
     authorize @experience
-    @card = Card.where(user_id: current_user.id).where(place_id: @experience.place_id)
-    authorize @card
-
-    if @card.nil?
-      @card.user_id = current_user.id
-      @card.place_id = @experience.place_id
-      @card.save!
-    else
-      @card = Card.where(user_id: current_user.id).where(place_id: @experience.place_id).last
-    end
-
     if @review.save
-      @experience.average_rating = (@experience.average_rating + @review.rating) / @experience.reviews.count
-      @experience.save!
-      if @card.stamp_count < 5
-        @card.stamp_count += 1
-      end
-      @card.save!
+      experience_avg
+      card_stamp
       redirect_to experience_path(@experience)
     else
       render :new
@@ -62,5 +47,37 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:content, :rating, :compliment_id, :booking_id)
+  end
+
+  def experience_avg
+    @experience.average_rating = (@experience.average_rating + @review.rating) / @experience.reviews.count
+    @experience.save!
+  end
+
+  def card_stamp
+    @cards = Card.where(user_id: current_user.id).where(place_id: @experience.place_id)
+    @card = Card.new
+    authorize @card
+    @card.user_id = current_user.id
+    @card.place_id = @experience.place_id
+    if @cards.last.state.active? || @cards.nil?
+      @card.stamp_count = 1
+      @card.active!
+    elsif @cards.last.completed?
+      @new_card = Card.create!(
+        user_id: @card.user_id,
+        place_id: @card.place_id,
+        stamp_count: 1
+      )
+      @new_card.save!
+      @new_card.active!
+    else
+      @card = Card.where(user_id: current_user.id).where(place_id: @experience.place_id).last
+      if @card.stamp_count < 5
+        @card.stamp_count += 1
+        @card.completed! if @card.stamp_count == 5
+      end
+    end
+    @card.save!
   end
 end
